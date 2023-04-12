9b4454da35395fc1727d7c5cc4f42f680a68c81d
Kristina Mnatsakanian <kristina.m1999@yandex.ru>
2023-04-13 02:29:06 +0600

    feat: add solutions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,20 +6,25 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(!sampleActivity) return false;
+  if (isNaN(+(sampleActivity))) return false;
+  if (typeof sampleActivity === "number" || typeof sampleActivity === 'undefined' || typeof sampleActivity === 'object') return false;
+  if (sampleActivity > 15 || sampleActivity <= 0) return false;
+
+  const k = Math.log(2) / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity)) / k)
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -3,19 +3,36 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('\'arr\' parameter must be an instance of the Array!');
+  const transArr = arr.slice();
+  for (let i = 0; i < transArr.length; i++) {
+    if (transArr[i] === '--discard-next') {
+      transArr[i + 1] = null;
+      transArr[i] = null;
+    }
+    if (transArr[i] === '--discard-prev') {
+      transArr[i - 1] = null;
+      transArr[i] = null;
+    }
+    if (transArr[i] === '--double-prev') {
+      transArr[i] = transArr[i - 1];
+    }
+    if (transArr[i] === '--double-next') {
+      transArr[i] = transArr[i + 1];
+    }
+  }
+  return transArr.filter(el => el !== null && el !== undefined);
 }
 
 module.exports = {
